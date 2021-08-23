ea5e3abd2408852b81e76b694be4cefb1538fe62
Sterling J <65771567+SterlingJohnston@users.noreply.github.com>
2021-08-23 19:58:41 -0400

    Update BMO.py

--- BMO.py
+++ BMO.py
@@ -1,5 +1,6 @@
 import discord
 import os
+import asyncio
 from dotenv import load_dotenv
 from discord.ext import commands
 from discord.ext.commands import Bot
@@ -12,7 +13,7 @@ client = commands.Bot(command_prefix = '!', intents=intents)
 load_dotenv()
 TOKEN = os.getenv("TOKEN")
 
-print("Bot made by Indy12323#1203")
+print("Bot made by Indysaurus#0001")
 
 #Reads config file and splits into dictionary.
 config = {}
@@ -21,16 +22,18 @@ with open('config.txt') as fileobj:
       key, value = line.rstrip("\n").split("=")
       config[key] = value
 
-
 #Prints config to console.
 print("Printing bot configurations...")
 for key,val in config.items():
     print (key, "=>", val)
 
 #Reads nono-words.txt the list of banned words and puts into a list + lowercase.
-nono = open('nono-words.txt')
-bad_words = [line.strip() for line in nono]
-bad_words = [x.lower() for x in bad_words]
+def nono():
+    global bad_words
+    test = []
+    with open ('nono-words.txt', 'r+') as f:
+        bad_words = f.read().splitlines()
+        bad_words = [x.lower() for x in bad_words]
 
 #Ready Check
 print("\nAttempting to log in...")
@@ -49,30 +52,37 @@ if ('Purge', 'True') in config.items():
     @client.command()
     @commands.has_permissions(manage_messages = True)
     async def purge(ctx, number):
-        print("2")
         number = int(number)
         number += 1
         if number <= 0:
             return
-        print("3")
         await ctx.channel.purge(limit=number)
+
 
 #Chat filter. Filter located in nono_words
 if ('Filter', 'True') in config.items():
     @client.event
     async def on_message(message):
+        print(message)
+        messageChannel = message.channel.id
         messageContent = message.content.lower()
-        if len(messageContent) > 0:
+        if len(messageContent) > 0 and message.channel.id != int(config.get('BotLogs')):
             if any(bad_word in messageContent for bad_word in bad_words):
-                    await message.delete()
-                    await message.channel.send('Thats a nono word.')
+                await message.delete()
+                channel = client.get_channel(int(config.get('BotLogs')))
+                report =(message.author.id, messageContent)
+                await channel.send(report)
+                msg = await message.channel.send('A filtered word was used. This incident has been logged.')
+                await asyncio.sleep(8) #seconds
+                await msg.delete()
         await client.process_commands(message)
-
+        
 if ('AutoRole', 'True') in config.items():
     @client.event
     async def on_member_join(member):
         roleAssign = get(member.guild.roles, name=config.get('RoleName'))
         await member.add_roles(roleAssign)
 
-
-client.run(TOKEN)
+if __name__ == "__main__":
+    nono()
+    client.run(TOKEN)
